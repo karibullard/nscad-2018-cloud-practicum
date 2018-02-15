e8f6e8f6ed217486279bb4d520f6917bba4f9c10
Olga <sirotinsky206@hotmail.com>
2018-02-14 21:36:32 -0800

    adjusted the mongocontext file by removing hard coded strings

--- TaskManagement/App_Start/MongoContext.cs
+++ TaskManagement/App_Start/MongoContext.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
-using API.Models; 
+using API.Models;
+using System.Configuration; 
 
 namespace TaskManagement.App_Start
 {
@@ -7,20 +8,29 @@ namespace TaskManagement.App_Start
     //this class connects to mongo db using MongoDb driver
     public class MongoContext
     {
-        public IMongoDatabase DataBase; 
+        /// <summary>
+        ///This is an object of IMongoDB
+        /// </summary>
+        public IMongoDatabase DataBase;
 
+        /// <summary>
+        ///This constructs the context with new client and database
+        /// </summary>
         public MongoContext()
         {
+
+
+
             //create an instance of mongo client using connection string to mongo TaskManager db
-            var client = new MongoClient("mongodb://localhost:27017/TaskManager");
+            var mongoClient = new MongoClient(ConfigurationManager.AppSettings["MongoDbHost"]);
+           
             //get TaskManager database from client connection 
-            DataBase = client.GetDatabase("TaskManager"); 
+            DataBase = mongoClient.GetDatabase(ConfigurationManager.AppSettings["MongoDbName"]);
 
 
         }
-        //retrieve  user collection from database
-        public IMongoCollection<User> Users => DataBase.GetCollection<User>("user_main"); 
-
-
+        //retrieve  user collection from database change this based on wanted collection
+        public IMongoCollection<User> Users => DataBase.GetCollection<User>("user_main");       
+       
     }//end class TaskManagerContext
 }//end namespace
