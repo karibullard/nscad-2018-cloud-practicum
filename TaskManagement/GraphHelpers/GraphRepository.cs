b44fc40f54b083b9daef08a76bffd550ff0e071a
Jason Drew Panzer <jason.drew.panzer@gmail.com>
2018-03-12 11:15:39 -0700

    Adding Graph library helper classes

---
+++ TaskManagement/GraphHelpers/GraphRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace API.GraphHelpers
+{
+    public class GraphRepository
+    {
+        // Get the current user's email address from their profile.
+        public async Task<string> GetMyEmailAddress(GraphServiceClient graphClient)
+        {
+
+            // Get the current user. 
+            // This sample only needs the user's email address, so select the mail and userPrincipalName properties.
+            // If the mail property isn't defined, userPrincipalName should map to the email for all account types. 
+            User me = await graphClient.Me.Request().Select("mail,userPrincipalName").GetAsync();
+            return "Found user!" ?? "No user!";
+        }
+    }
+}
---
+++ TaskManagement/GraphHelpers/SDKHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http.Headers;
+using Microsoft.Graph;
+
+namespace API.GraphHelpers
+{
+    /// <summary>
+    /// SDKHelper allows the api app to request a token to grant access to the Graph API.
+    /// Portions of this code copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+    /// </summary>
+    public class SDKHelper
+    {
+        private static GraphServiceClient graphClient = null;
+
+        // Get an authenticated Microsoft Graph Service client.
+        public static GraphServiceClient GetAuthenticatedClient()
+        {
+            GraphServiceClient graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        string accessToken = await SampleAuthProvider.Instance.GetUserAccessTokenAsync();
+
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+                    }));
+            return graphClient;
+        }
+
+        public static void SignOutClient()
+        {
+            graphClient = null;
+        }
+    }
+}
